e270a66aadded18611b3884fc5ef72d22775eba5
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2023-10-13 17:39:52 +0200

    cd into src directory if no argument specified.

--- scripts/cd.sh
+++ scripts/cd.sh
@@ -11,6 +11,8 @@ if _TMP_TARGET_DIR=$(_tuda_wss_get_workspace_root); then
     fi
     _TMP_TARGET_DIR=$(colcon list --base-paths "$_TMP_TARGET_DIR" --packages-select $1 --paths-only 2>/dev/null)
     unset _TMP_COLCON_ERROR
+  elif [ -d "$_TMP_TARGET_DIR/src" ]; then
+    _TMP_TARGET_DIR="$_TMP_TARGET_DIR/src"
   fi
   cd "$_TMP_TARGET_DIR" || return
 fi
