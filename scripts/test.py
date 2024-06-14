d9a6d0c796327232edded75dddb229782eaa70a1
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-06-14 15:29:33 +0200

    Fixed --this argument in test script.

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 from tuda_workspace_scripts.print import print_error, print_info
-from tuda_workspace_scripts.workspace import get_workspace_root, PackageChoicesCompleter
+from tuda_workspace_scripts.workspace import find_packages_in_directory, get_workspace_root, PackageChoicesCompleter
 import argcomplete
 import argparse
 import os
@@ -21,19 +21,27 @@ if __name__ == '__main__':
     if workspace_root is None:
         print_error('You are not in a workspace!')
         exit()
+
+    packages = args.packages or []
+    if args.this:
+        packages = find_packages_in_directory(os.getcwd())
+        if len(packages) == 0:
+            print_error("No package found in the current directory!")
+            exit(1)
+
     os.chdir(workspace_root)
     print_info('>>> Building packages')
-    if args.packages is not None and len(args.packages) > 0:
-        command = subprocess.run('colcon build --packages-select ' + ' '.join(args.packages),
+    if len(packages) > 0:
+        command = subprocess.run('colcon build --packages-select ' + ' '.join(packages),
                                  stdout=sys.stdout, stderr=sys.stderr, shell=True)
         if command.returncode != 0:
             print_error('>>> Failed to build packages')
             exit(command.returncode)
         print_info('>>> Running tests')
-        command = subprocess.run('colcon test --packages-select ' + ' '.join(args.packages),
+        command = subprocess.run('colcon test --packages-select ' + ' '.join(packages),
                                  stdout=sys.stdout, stderr=sys.stderr, shell=True)
         returncode = command.returncode
-        for package in args.packages:
+        for package in packages:
             print_info(f'>>> {package}')
             command = subprocess.run(f'colcon test-result --verbose --test-result-base build/{package}',
                                      stdout=sys.stdout, stderr=sys.stderr, shell=True)
