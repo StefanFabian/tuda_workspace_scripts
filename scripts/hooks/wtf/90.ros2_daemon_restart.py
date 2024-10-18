34f754158c73ccbf3ea0d8bde6dd86a4bd76ad14
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-10-18 08:54:39 +0200

    Use python api to restart ros2 daemon and handle keyboard interrupts.

--- scripts/hooks/wtf/90.ros2_daemon_restart.py
+++ scripts/hooks/wtf/90.ros2_daemon_restart.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
-from tuda_workspace_scripts.print import print_header, print_info
+from tuda_workspace_scripts.print import *
+from ros2cli.node.daemon import is_daemon_running, spawn_daemon, shutdown_daemon
 import os
 
 
 def fix():
-    print_header("Restarting ROS2 daemon")
-    os.system("ros2 daemon stop")
-    os.system("ros2 daemon start")
-    print_info("ROS2 daemon restarted")
-    return False  # This should never end the fix chain
+    print_header("Checking ROS2 daemon")
+    while True:
+        try:
+            if is_daemon_running(args=[]):
+                print_info("ROS2 daemon is running. Restarting it just to be safe.")
+                if not shutdown_daemon(args=[], timeout=10):
+                    print_error("Failed to shutdown ROS2 daemon")
+                    return False
+                if not spawn_daemon(args=[], timeout=10):
+                    print_error("Failed to restart ROS2 daemon after stopping it")
+                    return False
+                print_info("ROS2 daemon restarted")
+                return False
+            print_info("ROS2 daemon is not running. Starting it.")
+            if not spawn_daemon(args=[], timeout=10):
+                print_error("Failed to start ROS2 daemon")
+                return False
+            # The ros2 daemon not running could have actually been an issue for commands such as ros2 topic/service/action/...
+            return True
+        except KeyboardInterrupt:
+            print_warn(
+                "Canceling the restart of the ROS2 daemon might lead to further issues."
+            )
+            if confirm("Stop anyway?"):
+                raise
+            print_info("Okay. Trying again.")
