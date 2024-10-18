4182092e6c1ca9922080b3f6c7835bfc7878367f
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-10-18 08:54:53 +0200

    Handle keyboard interrupts in wtf.

--- scripts/wtf.py
+++ scripts/wtf.py
@@ -2,7 +2,7 @@
 # PYTHON_ARGCOMPLETE_OK
 import argparse
 import argcomplete
-from tuda_workspace_scripts.print import print_error, print_info, confirm
+from tuda_workspace_scripts.print import *
 from tuda_workspace_scripts.scripts import get_hooks_for_command
 import importlib.util
 
@@ -15,25 +15,30 @@ def load_method_from_file(file_path: str, method_name: str):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--continue-on-fix",
-        default=False,
-        action="store_true",
-        help="Continue running fixes even if one fix was successful.",
-    )
-    argcomplete.autocomplete(parser)
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--continue-on-fix",
+            default=False,
+            action="store_true",
+            help="Continue running fixes even if one fix was successful.",
+        )
+        argcomplete.autocomplete(parser)
+        args = parser.parse_args()
 
-    continue_on_fix = args.continue_on_fix
-    fixed_something = False
-    # Collect all wtf scripts in hooks/wtf folders of TUDA_WSS_SCRIPTS environment variable
-    for script in sorted(get_hooks_for_command("wtf")):
-        if fixed_something and not continue_on_fix:
-            if confirm(f"This might have fixed your issue! Continue anyway?"):
-                break
-            continue_on_fix = True
-        # Load script and run fix command and obtain result
-        fix = load_method_from_file(script, "fix")
-        if fix():
-            fixed_something = True
+        continue_on_fix = args.continue_on_fix
+        fixed_something = False
+        # Collect all wtf scripts in hooks/wtf folders of TUDA_WSS_SCRIPTS environment variable
+        for script in sorted(get_hooks_for_command("wtf")):
+            if fixed_something and not continue_on_fix:
+                if confirm(f"This might have fixed your issue! Continue anyway?"):
+                    break
+                continue_on_fix = True
+            # Load script and run fix command and obtain result
+            fix = load_method_from_file(script, "fix")
+            if fix():
+                fixed_something = True
+    except KeyboardInterrupt:
+        print_warn("Stopping per user request.")
+        print("Good bye.")
+        exit(0)
