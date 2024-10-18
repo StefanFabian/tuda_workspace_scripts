db2067d9ef7d32deea383c1e30cb82f9abe451dc
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-10-18 08:54:19 +0200

    Add scripts functions to general tuda_workspace_scripts python lib.

--- tuda_workspace_scripts/__init__.py
+++ tuda_workspace_scripts/__init__.py
@@ -1,4 +1,5 @@
 from .build import *
 from .config import CONFIG_FILE_PATH, Config, load_config, load_variable, load_variables, Variable
 from .print import *
+from .scripts import *
 from .workspace import *
